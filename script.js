bb18f1b43abede1006cbde1c82ed1f556f21254c
Patrick Goulding <patrickgoulding@gmail.com>
2019-02-07 15:51:25 -0700

    bare bones site organization

---
+++ script.js
@@ -0,0 +1,11 @@
+/*---------- Query Selectors -----------*/
+var whatevs = document.querySelector('.whatevs');
+
+/*---------- Global Variables ----------*/
+var example = ;
+
+/*---------- Event Listeners -----------*/
+*.addEventListener('click', )
+
+/*---------- Functions -----------------*/
+function
